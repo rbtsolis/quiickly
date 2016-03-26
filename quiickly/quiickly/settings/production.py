7ff47efcb912b26d6af5435b4b9aae234d253215
Roberth Solís Martínez <roberth.solis@gmail.com>
2016-03-26 14:23:30 -0600

    alloweb host added

--- quiickly/quiickly/settings/production.py
+++ quiickly/quiickly/settings/production.py
@@ -3,7 +3,7 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['quiickly.co']
+ALLOWED_HOSTS = ['quiickly.co','149.56.14.136']
 
 '''
 DATABASES = {
