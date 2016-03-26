5a6aba05f72aaa3b894e176899de3340c76a8671
Roberth Solís Martínez <roberth.solis@gmail.com>
2016-03-26 16:09:37 -0600

    final db postgres test

--- quiickly/quiickly/settings/production.py
+++ quiickly/quiickly/settings/production.py
@@ -5,30 +5,29 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['quiickly.co','149.56.14.136']
 
-'''
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'quiicklytest',
-        'PASSWORD': 'quiicklytest',
+        'NAME': 'quiickly2db',
+        'USER': 'quiickly2',
+        'PASSWORD': 'aistream',
         'HOST': 'localhost',
         'PORT': '',
     }
 }
+
+
+
 '''
-
-
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': dirname(join(BASE_DIR, 'db.sqlite3')),
     }
 }
+'''
 
-DB_RBT = dirname(join(BASE_DIR, 'db.sqlite3'))
-
-print(DB_RBT)
 
 '''
 
