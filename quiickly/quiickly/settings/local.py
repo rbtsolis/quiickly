47a1f03c9c554a5e5a46e81c55e57d7362f9c7e4
Roberth Solís Martínez <roberth.solis@gmail.com>
2016-03-26 07:00:17 -0600

    settings of local enviroment

---
+++ quiickly/quiickly/settings/local.py
@@ -0,0 +1,38 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+# Database
+# https://docs.djangoproject.com/en/1.9/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+
+'''
+
+Static files configurations
+
+'''
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+
+STATIC_URL = '/static/'
+
+
+STATICFILES_DIRS = (
+    join(BASE_DIR, 'static'),
+)
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = join(BASE_DIR, 'media')
+
