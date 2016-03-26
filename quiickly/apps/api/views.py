eff34238f92011cb5031a547baadcf0eb9a24a47
Roberth Solís Martínez <roberth.solis@gmail.com>
2016-03-26 06:57:18 -0600

    views of api app

---
+++ quiickly/apps/api/views.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+'''
+This is an API of quickly team
+
+'''
+
+from django.http import HttpResponse
+from rest_framework.decorators import permission_classes
+from rest_framework import permissions, viewsets
+from apps.products.serializers import ProductSerializer
+from apps.products.models import Product
+
+
+'''
+
+This is a function to get all details of the product
+
+'''
+@permission_classes((permissions.IsAuthenticated, ))
+class ProductViewSet(viewsets.ReadOnlyModelViewSet):
+
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
