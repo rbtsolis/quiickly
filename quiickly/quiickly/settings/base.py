4bd49662cafa084e65fed69c32b0e2fdae950afa
Roberth Solís Martínez <roberth.solis@gmail.com>
2016-03-27 17:14:48 -0600

    cors added

--- quiickly/quiickly/settings/base.py
+++ quiickly/quiickly/settings/base.py
@@ -57,6 +57,7 @@ INSTALLED_APPS = DJANGO_APPS + LOCAL_APPS + THIRD_PARTY_APPS
 MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
