771d1c024d1aa60b17e4c6f88a0486eb45b54754
Roberth Solís Martínez <roberth.solis@gmail.com>
2016-03-27 18:45:09 -0600

    base modified

--- quiickly/quiickly/settings/base.py
+++ quiickly/quiickly/settings/base.py
@@ -132,7 +132,7 @@ REST_FRAMEWORK = {
     # or allow read-only access for unauthenticated users.
 
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+        'rest_framework.permissions.AllowAny',
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': (
         #'rest_framework.authentication.BasicAuthentication',
