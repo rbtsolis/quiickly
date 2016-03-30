6c68dfe4dae2cc4dcdd4df5d85a476568cf54705
Roberth Solís Martínez <roberth.solis@gmail.com>
2016-03-29 18:41:07 -0600

    Templates loaders fast on production settings

--- quiickly/quiickly/settings/production.py
+++ quiickly/quiickly/settings/production.py
@@ -30,6 +30,24 @@ DATABASES = {
 
 
 '''
+Templates Configuration Section
+Cache the templates in memory for speed-up
+
+'''
+loaders = [
+    (
+        'django.template.loaders.cached.Loader',
+        [
+            'django.template.loaders.filesystem.Loader',
+            'django.template.loaders.app_directories.Loader',
+        ]
+    ),
+]
+
+TEMPLATES[0]['OPTIONS'].update({"loaders": loaders})
+
+
+'''
 
 Static files configurations on Amazon S3
 
