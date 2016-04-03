a810fd0cb0fab6949f45f6a56a28d11a0356a98f
Roberth Solís Martínez <roberth.solis@gmail.com>
2016-04-03 10:46:43 -0600

    urls modified

--- quiickly/apps/api/urls.py
+++ quiickly/apps/api/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url, include
 from rest_framework import routers
-from .views import ProductViewSet
+from .views import ProductViewSet, OrderViewSet
 
 router = routers.SimpleRouter()
 router.register(r'products', ProductViewSet)
+router.register(r'orders', OrderViewSet)
 
 urlpatterns = [
     url(r'^', include(router.urls)),
