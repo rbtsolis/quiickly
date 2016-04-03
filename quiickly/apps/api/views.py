7c8e70d26aa5f0dd0ffd338b990bb9bc334d2163
Roberth Solís Martínez <roberth.solis@gmail.com>
2016-04-03 10:46:52 -0600

    Order Added

--- quiickly/apps/api/views.py
+++ quiickly/apps/api/views.py
@@ -6,8 +6,8 @@ This is an API of quickly team
 '''
 from rest_framework.decorators import permission_classes
 from rest_framework import permissions, viewsets
-from apps.products.serializers import ProductSerializer
-from apps.products.models import Product
+from apps.products.serializers import ProductSerializer, OrderSerializer
+from apps.products.models import Product, Order
 
 
 '''
@@ -15,9 +15,15 @@ from apps.products.models import Product
 This is a function to get all details of the product
 
 '''
-#@permission_classes((permissions.IsAuthenticated, ))
 class ProductViewSet(viewsets.ReadOnlyModelViewSet):
 
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+
+#@permission_classes((permissions.IsAuthenticated, ))
+class OrderViewSet(viewsets.ModelViewSet):
+
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
 
