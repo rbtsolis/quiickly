9656f59b45084d90d0565e36e7664bca99d2c8ca
Roberth Solís Martínez <roberth.solis@gmail.com>
2016-04-10 15:12:17 -0600

    jwt deleted, restframework modified

--- quiickly/quiickly/settings/base.py
+++ quiickly/quiickly/settings/base.py
@@ -137,8 +137,6 @@ Auth Users Configuraation
 '''
 AUTH_USER_MODEL = 'users.User'
 
-
-
 THUMBNAIL_EXTENSION = 'png'
 
 
@@ -148,11 +146,8 @@ Django Rest Framework Configuration Section
 
 '''
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.AllowAny',
-    ],
+
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
     )
@@ -194,40 +189,3 @@ CORS_URLS_REGEX = r'^/api/v1/.*$'
 CORS_ALLOW_METHODS = [
     'GET',
 ]
-
-
-
-'''
-JWT(Json Web Tokens Configuration)
-
-'''
-JWT_AUTH = {
-    'JWT_ENCODE_HANDLER':
-    'rest_framework_jwt.utils.jwt_encode_handler',
-
-    'JWT_DECODE_HANDLER':
-    'rest_framework_jwt.utils.jwt_decode_handler',
-
-    'JWT_PAYLOAD_HANDLER':
-    'rest_framework_jwt.utils.jwt_payload_handler',
-
-    'JWT_PAYLOAD_GET_USER_ID_HANDLER':
-    'rest_framework_jwt.utils.jwt_get_user_id_from_payload_handler',
-
-    'JWT_RESPONSE_PAYLOAD_HANDLER':
-    'apps.api.tokens.jwt_response_payload_handler',
-
-    'JWT_SECRET_KEY': SECRET_KEY,
-    'JWT_ALGORITHM': 'HS512',
-    'JWT_VERIFY': True,
-    'JWT_VERIFY_EXPIRATION': True,
-    'JWT_LEEWAY': 0,
-    'JWT_EXPIRATION_DELTA': datetime.timedelta(days=7),
-    'JWT_AUDIENCE': None,
-    'JWT_ISSUER': None,
-
-    'JWT_ALLOW_REFRESH': False,
-    'JWT_REFRESH_EXPIRATION_DELTA': datetime.timedelta(days=7),
-
-    'JWT_AUTH_HEADER_PREFIX': 'JWT',
-}
