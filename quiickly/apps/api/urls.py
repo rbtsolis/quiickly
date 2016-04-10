7d394d4600f5d2c69b15bd90bc9fa1a1c04981c9
Roberth Solís Martínez <roberth.solis@gmail.com>
2016-04-10 15:11:08 -0600

    urls api modified

--- quiickly/apps/api/urls.py
+++ quiickly/apps/api/urls.py
@@ -1,11 +1,14 @@
 from django.conf.urls import url, include
 from rest_framework import routers
 from .views import ProductViewSet, OrderViewSet
+from .tokens import obtain_auth_token
+from rest_framework_jwt.views import obtain_jwt_token
 
 router = routers.SimpleRouter()
 router.register(r'products', ProductViewSet)
 router.register(r'orders', OrderViewSet)
 
 urlpatterns = [
-    url(r'^', include(router.urls)),
+    url(r'^v1/tokens/create/$', obtain_auth_token),
+    url(r'^v1/', include(router.urls)),
 ]
