f9001c4a0a3f2456de3dbfa1bb1dbf383cfa97aa
Roberth Solís Martínez <roberth.solis@gmail.com>
2016-04-15 08:56:11 -0600

    urls changed

--- quiickly/apps/api/urls.py
+++ quiickly/apps/api/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import url, include
 from rest_framework import routers
-from .views import ProductViewSet, OrderViewSet
+from .views import ProductViewSet, OrderViewSet, UserViewSet, AddressViewSet
 from .tokens import obtain_auth_token
 
 router = routers.SimpleRouter()
 router.register(r'products', ProductViewSet)
 router.register(r'orders', OrderViewSet)
+router.register(r'users', UserViewSet)
+router.register(r'address', AddressViewSet)
 
 urlpatterns = [
     url(r'^v1/tokens/create/$', obtain_auth_token),
