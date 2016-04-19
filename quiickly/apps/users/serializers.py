0c0a8fc3af3a0b431d7eddba5425cb99080de156
Roberth Solís Martínez <roberth.solis@gmail.com>
2016-04-19 08:48:04 -0600

    Serializers of users changed

--- quiickly/apps/users/serializers.py
+++ quiickly/apps/users/serializers.py
@@ -10,32 +10,76 @@ from decimal import Decimal
 from apps.users.models import User, Address
 
 
-class UserSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = User
-        fields = (
-            'id','display_name', 'email', 'avatar', 'phone', 'address'
-        )
-        depth = 1
-
-
 class AddressSerializer(serializers.ModelSerializer):
 
     class Meta:
 
         model = Address
+        fields = '__all__'
+
+
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
         fields = (
-            'id', 'name', 'address', 'latitude', 'longitude'
+            'display_name', 'email', 'avatar', 'phone', 'address'
         )
+        depth = 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 class UserCreateSerializer(serializers.ModelSerializer):
 
+    address = AddressSerializer()
 
     class Meta:
 
         model = User
         fields = (
-            'display_name', 'password', 'email', 'avatar', 'phone', 'os'
+            'display_name', 'email', 'password', 'avatar', 'phone', 'os', 'address'
         )
+        depth = 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    '''
+    def create(self, validated_data):
+
+        address = dict(validated_data['address'])
+
+        address_created = Address.objects.create(**address)
+
+        del validated_data['address']
+
+        user = User.objects.create(**validated_data)
+
+        user.address.add(address_created)
+        print("--------------------------------------------")
+        print(user)
+        return {'created','true'}
+    '''
