5c3963d8f1eb67b6c0a558b6878f2542cf80f4a7
Roberth Solís Martínez <roberth.solis@gmail.com>
2016-04-19 08:47:24 -0600

    views of store

--- quiickly/apps/store/views.py
+++ quiickly/apps/store/views.py
@@ -4,13 +4,4 @@ from apps.products.models import Product
 
 
 def list_products(request):
-
-    products = Product.objects.all()
-
-    products_list = {
-
-        "products" : products
-
-    }
-
-    return render(request, "products.html", products_list)
+    return render(request, "products.html")
